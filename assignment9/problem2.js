ccd4de51f4a4cfb208b7d5f9be4feed0cee7ddcb
diyvanshu <skprogrammer1007@gmail.com>
2022-12-18 15:40:28 +0530

    assign ment 9

---
+++ assignment9/problem2.js
@@ -0,0 +1,1 @@
+// When you declare a variable in a function, you can only access it in the function. These variables are said to be scoped to the function. If you define any inner function within another function, this inner function is called a closure. It retains access to the variables created in the outer function.28-Aug-2017
---
+++ assignment9/problem4.js
@@ -0,0 +1,9 @@
+for (var i = 0; i < 3; i++) {
+    setTimeout(function log() {
+        console.log(i); // What is logged?
+    }, 1000);
+}
+// 3
+// 3
+// 3
+// this is output of the code 
